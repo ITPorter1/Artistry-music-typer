ae77ea71528ed182d0a952a905544d8813e939bd
isaac <isaac@CCLs-MacBook-Air.local>
2022-03-24 14:55:12 -0400

    array of artist names implemented

--- Artistry.js
+++ Artistry.js
@@ -10,3 +10,32 @@ const scoreDisplay = document.querySelector('#score');
 const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
+
+const names = [
+    'Kendrick Lamar',
+    'Chance The Rapper',
+    'Jay Z',
+    'J.Cole',
+    'Drake',
+    'Kanye West',
+    'Lil Wayne',
+    'Method Man',
+    'DMX',
+    'Eminem',
+    'Westside Boogie',
+    'Russ',
+    'Stove God Cooks',
+    'Da Baby',
+    '50 Cent',
+    'JID',
+    'Schoolboy Q',
+    'Nas',
+    'Vince Staples',
+    'Ludacris',
+    'Saba',
+    'Smino',
+    'Biggie Smalls',
+    'Royce Da 59',
+    'Pusha T'
+  ];
+  
